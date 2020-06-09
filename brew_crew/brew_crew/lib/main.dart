01fa0469fc8b9d90f806300d972b505d517f26e8
kouyoumikan <kouyoumikan136.gmail.com>
2020-06-09 11:09:43 +0900

    修正

--- brew_crew/brew_crew/lib/main.dart
+++ brew_crew/brew_crew/lib/main.dart
@@ -7,8 +7,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Wrapper(),
+    return StreamProvider(
+          child: MaterialApp(
+        home: Wrapper(),
+      ),
     );
   }
 }
