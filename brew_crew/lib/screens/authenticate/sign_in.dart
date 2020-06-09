18126ce4e81d6224612b4469c98caefc7bd0434c
kouyoumikan <kouyoumikan136.gmail.com>
2020-06-09 11:05:13 +0900

    ソース修正

--- brew_crew/lib/screens/authenticate/sign_in.dart
+++ brew_crew/lib/screens/authenticate/sign_in.dart
@@ -21,24 +21,24 @@ class _SignInState extends State<SignIn> {
         title: Text("Sign in Brew Crew"),
       ),
       body: Container(
-          padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-          child: RaisedButton(
-            child: Text("sign in anon"),
-            onPressed: () async {
-               // auth.dartで取得した匿名ログイン結果を非同期で呼び出す
-               // 匿名ログインが失敗時にはno、成功時にはdatabasaユーザーとしてデータを返す
-              dynamic result = await _auth.signInAnon();
-              // 取得した匿名ログイン結果の判定
-              if(result == null){
-                print("error signing in");// ログイン失敗時にconsoleに出力
-              }
-              else{
-                print("signed in"); // ログイン成功時にconsoleに出力
-                print(result.uid); // 取得した匿名ログイン結果を出力
-              }
-            },
-          ),
+        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+        child: RaisedButton(
+          child: Text("sign in anon"),
+          onPressed: () async {
+              // auth.dartで取得した匿名ログイン結果を非同期で呼び出す
+              // 匿名ログインが失敗時にはno、成功時にはdatabasaユーザーとしてデータを返す
+            dynamic result = await _auth.signInAnon();
+            // 取得した匿名ログイン結果の判定
+            if(result == null){
+              print("error signing in");// ログイン失敗時にconsoleに出力
+            }
+            else{
+              print("signed in"); // ログイン成功時にconsoleに出力
+              print(result.uid); // 取得した匿名ログイン結果を出力
+            }
+          },
         ),
+      ),
     );
   }
 }
