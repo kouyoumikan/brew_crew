e79231f73ec8894c62db7e3480f8d7200848176f
kouyoumikan <kouyoumikan136.gmail.com>
2020-06-11 15:55:06 +0900

    #9　ホーム画面にアプリバー作成

--- brew_crew/lib/screens/home/home.dart
+++ brew_crew/lib/screens/home/home.dart
@@ -3,8 +3,14 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("home"),
+    return Scaffold(
+      backgroundColor: Colors.brown[50],
+      appBar: AppBar(
+        title: Text("Brew Crew"),
+        backgroundColor: Colors.brown[400],
+        elevation: 0.0,
+        actions: <Widget>[],
+      ),
     );
   }
 }
